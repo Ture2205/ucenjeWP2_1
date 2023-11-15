5b92feafc1fe57421010dbe36a2c66e903f3ed89
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2023-11-15 15:36:45 +0100

    doktorska_ordinacija

---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+--create database edunovaWP2;
+
+--create table doktor(
+--ime varchar(50),
+--odjel varchar(50),
+--specijalizacija varchar(50)
+--);
+
+
+
+--create table pacijent(
+--ime varchar(50),
+--odjel varchar(50),
+--vrstaozljede varchar(50),
+--trajanjeoporavka datetime
+--);
+
+
+--create table medicinskasestra(
+--ime varchar(50),
+--odjel varchar(50),
+--radnoiskustvo int
+--);
