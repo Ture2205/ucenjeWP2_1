e632661c2d8d2fdad753f54a5e63034155da1903
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2023-11-29 10:24:32 +0100

    sql tablica

---
+++ pacijent.sql
@@ -0,0 +1,19 @@
+--create database pacijent;
+
+--use pacijent;
+
+--create table pacijent(
+--ime varchar(20) not null,
+--prezime varchar(20) not null,
+--lijek varchar(30) not null,
+--datumpodizanja datetime,
+--oib int not null);
+
+--create table lijek(
+--sifra int not null,
+--ime varchar(30) not null,
+--brojtableta int not null,
+--datumpodizanja datetime,
+--brojpodizanja int not null);
+
+
