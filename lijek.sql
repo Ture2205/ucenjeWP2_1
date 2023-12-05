11b328362ae9f7eb5d0543d55f1de49a70531dea
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2023-12-05 17:28:50 +0100

    sql

---
+++ lijek.sql
@@ -0,0 +1,29 @@
+create database lijek;
+
+use lijek;
+
+create table pacijent(
+sifra int not null primary key,
+ime varchar(20) not null,
+prezime varchar(20) not null,
+datumroðenja datetime,
+spol varchar(20) not null,
+lijek varchar(30) not null
+);
+
+create table lijek(
+sifra int not null primary key,
+tip varchar(30) not null,
+doza int not null,
+brojtableta int not null,
+nacinprimjene varchar(20),
+datumpodizanja datetime
+);
+
+select *from lijek;
+
+select *from pacijent;
+
+insert into pacijent values('1001', 'Mario','Turèek','1995-05-22','M','Eutyrox')
+select *from lijek;
+insert into lijek values ('1001', 'na recept','1', '52', 'oralno', '2023-11-27')
