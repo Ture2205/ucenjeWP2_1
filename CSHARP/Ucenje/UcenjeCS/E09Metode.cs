f1e88b7545464397cb0f79100cb602fad9378947
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2024-01-15 21:17:02 +0100

    push

---
+++ CSHARP/Ucenje/UcenjeCS/E09Metode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E09Metode
+    {
+        
+
+        {
+            return Zbroji(PrviBroj, DrugiBroj);
+
+    }
+
+    private int Zbroji(int prviBroj, int drugiBroj)
+            
+            }
+return prviBroj + drugiBroj;
+        }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//09Metode.Izvedi();
+
+Console.WriteLine(Z01Metode.CijeliDioZbroja(3.4, 5.8));
